ba6497613a12040125d7ee64962d1e82dc85fb25
Reid Delaney <rdelaney1923@gmail.com>
2015-09-24 03:28:01 -0400

    Commit of final Geo Quiz App

---
+++ GeoQuiz/app/src/main/java/com/delaney/geoquiz/Question.java
@@ -0,0 +1,31 @@
+package com.delaney.geoquiz;
+
+/**
+ * Created by Reid on 9/24/2015.
+ */
+public class Question {
+
+    private int mTextResId;
+    private boolean mAnswerTrue;
+
+    public Question(int textResId, boolean answerTrue) {
+        mTextResId = textResId;
+        mAnswerTrue = answerTrue;
+    }
+
+    public int getTextResId() {
+        return mTextResId;
+    }
+
+    public void setTextResId(int textResId) {
+        mTextResId = textResId;
+    }
+
+    public void setAnswerTrue(boolean answerTrue) {
+        mAnswerTrue = answerTrue;
+    }
+
+    public boolean isAnswerTrue() {
+        return mAnswerTrue;
+    }
+}

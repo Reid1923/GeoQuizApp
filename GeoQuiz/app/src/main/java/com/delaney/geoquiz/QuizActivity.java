9cc14cd4e012a3b5b23ff3b81dcc9a10091872a1
Reid Delaney <rdelaney1923@gmail.com>
2015-09-29 05:12:32 -0400

    Second Challenge Completed
    Modified the QuizActivity Class and activity_quiz.xml to rectify the
    same issue as the first challenge except this time it was in the Quiz
    Activity instead of the Cheat Activity

--- GeoQuiz/app/src/main/java/com/delaney/geoquiz/QuizActivity.java
+++ GeoQuiz/app/src/main/java/com/delaney/geoquiz/QuizActivity.java
@@ -18,6 +18,7 @@ public class QuizActivity extends AppCompatActivity {
     private static final String TAG = "QuizActivity";
     private static final String KEY_INDEX = "index";
     private static final int REQUEST_CODE_CHEAT = 0;
+    private static final String KEY_CHEATER = "Cheat";
     private Button mTrueButton;
     private Button mFalseButton;
     private ImageButton mNextButton;
@@ -131,6 +132,7 @@ public class QuizActivity extends AppCompatActivity {
 
         if (savedInstanceState != null){
             mCurrentIndex = savedInstanceState.getInt(KEY_INDEX, 0);
+            mIsCheater = savedInstanceState.getBoolean(KEY_CHEATER);
         }
 
         updateQuestion();
@@ -141,6 +143,7 @@ public class QuizActivity extends AppCompatActivity {
         super.onSaveInstanceState(savedInstanceState);
         Log.i(TAG, "onSaveInstanceState");
         savedInstanceState.putInt(KEY_INDEX, mCurrentIndex);
+        savedInstanceState.putBoolean(KEY_CHEATER, true);
     }
 
     @Override
